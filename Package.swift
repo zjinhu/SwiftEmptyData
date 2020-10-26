4e54abd27465589a126bddb14155db57a3f5e011
Jackie <jackie>
2020-10-26 16:41:00 +0800

    SPM支持

--- Package.swift
+++ Package.swift
@@ -24,8 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftEmptyData",
-            dependencies: ["SnapKit"],
-            path: "SwiftEmptyData/SwiftEmptyData/Class")
+            dependencies: ["SnapKit"])
     ],
     swiftLanguageVersions: [
         .v5
