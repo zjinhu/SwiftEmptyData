0743bbb3d2a2e4f14795a8d7dd6531dc0b415b1d
jinhu zhang <814030966@qq.com>
2021-01-06 18:35:29 +0800

    release

--- SwiftEmptyData/SwiftEmptyData/AppDelegate.swift
+++ SwiftEmptyData/SwiftEmptyData/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
